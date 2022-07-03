325f4d8bfeb130c61857e512799992df05a54470
8string <bruno.grobelsek@hotmail.com>
2022-07-03 19:44:02 +0200

    first test

--- Aplikacija/app/src/static/js/app.js
+++ Aplikacija/app/src/static/js/app.js
@@ -53,7 +53,7 @@ function TodoListCard() {
         <React.Fragment>
             <AddItemForm onNewItem={onNewItem} />
             {items.length === 0 && (
-                <p className="text-center">Oh come on! Write something!</p>
+                <p className="text-center">Write something!</p>
             )}
             {items.map(item => (
                 <ItemDisplay
